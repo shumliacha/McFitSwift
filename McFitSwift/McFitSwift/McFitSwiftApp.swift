8eb3cb618f5eb3a2b223006f9ae8d9b8576a2924
shumliacha <126417371+shumliacha@users.noreply.github.com>
2024-08-17 00:10:21 +0200

    created initial project

---
+++ McFitSwift/McFitSwift/McFitSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  McFitSwiftApp.swift
+//  McFitSwift
+//
+//  Created by Kristina Litvinova on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct McFitSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
